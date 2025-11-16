2340e6c7c2fbb6aa92b52f59270de15d0123dd7c
Jorge Barnaby <jorge.barnaby@gmail.com>
2025-11-15 21:46:49 -0500

    fix: Fix when there is a custom base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  // Uncomment and set your base path when deploying to a subdirectory
+  // base: '/photos/',
   vite: {
     plugins: [tailwindcss()],
   },
